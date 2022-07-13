7d365bff2ac253a79b8fadeae59095ea74c9939f
S.R.N.Himavarsha <raghnisagineti07@gmail.com>
2022-07-13 13:55:43 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:cricker/login.dart';
-import 'package:cricker/register.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'login.dart';
+import 'register.dart';
+import 'buttons.dart';
+import 'SecondScreen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyLogin(),
-    routes: {
-      'register': (context) => MyRegister(),
-      'login': (context) => MyLogin(),
-    },
-  ));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: login(),
+    );
+  }
+}
+
