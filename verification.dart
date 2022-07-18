962cdc0375b4638e291ce346a5663ba8bfcedc96
S.R.N.Himavarsha <raghnisagineti07@gmail.com>
2022-07-18 12:13:02 +0530

    Update verification.dart

--- verification.dart
+++ verification.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:cricker/camera_page.dart';
+import 'camera_page.dart';
+import 'geotag.dart';
 
 class verification extends StatelessWidget {
   const verification({Key? key}) : super(key: key);
@@ -9,20 +10,42 @@ class verification extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await availableCameras().then(
-              (value) => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CameraPage(
-                    cameras: value,
-                  ),
-                ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () async {
+                  await availableCameras().then(
+                    (value) => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CameraPage(
+                          cameras: value,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                child: const Text('Launch Camera'),
               ),
-            );
-          },
-          child: const Text('Launch Camera'),
+            ),
+            SizedBox(
+              height: 50,
+              width: 200,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => geotag()),
+                  );
+                },
+                child: const Text('Get Location'),
+              ),
+            ),
+          ],
         ),
       ),
     );
