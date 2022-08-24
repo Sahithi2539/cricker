19c4806cc11467a6d854ce9532ead25202b36048
Vinnakota Lakshmi sahithi <69851403+Sahithi2539@users.noreply.github.com>
2022-08-24 09:58:28 +0530

    Add files via upload

---
+++ read data/get_user_name.dart
@@ -0,0 +1,12 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class GetUserName extends StatelessWidget {
+  const GetUserName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
